ec7dcdc4ad4c90c1c6cefd779a1cc352bfdd1a0c
Daniel Arndt Alves <progdan@gmail.com>
2014-04-14 12:18:02 -0300

    Exemplo: HelloAndroid - Versão 2.0 (entrada de dados do usuário)

--- HelloAndroid/src/com/progdan/helloandroid/MainActivity.java
+++ HelloAndroid/src/com/progdan/helloandroid/MainActivity.java
@@ -3,41 +3,37 @@ package com.progdan.helloandroid;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.os.Build;
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
+	private EditText nomeEditText;
+	private TextView saudacaoTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.main);
+        this.nomeEditText = (EditText) findViewById(R.id.nomeEditText);
+        this.saudacaoTextView = (TextView) findViewById(R.id.saudacaoTextView);
     }
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
+    public void surpreenderUsuario(View v) {
+    	// demais códigos existentes
+    	Editable texto = this.nomeEditText.getText();
+    	String saudacao = getResources().getString(R.string.saudacao);
+    	String msg = saudacao + " " + texto.toString();
+    	this.saudacaoTextView.setText(msg);
     }
 
 }
