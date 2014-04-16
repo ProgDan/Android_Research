0ac1cbaafb0ebaec2cc85b5e7d0cc8360ac6ded4
Daniel Arndt Alves <progdan@gmail.com>
2014-04-16 17:30:21 -0300

    Exemplo: BoaViagem - Criação da tela de cadastro de nova viagem

---
+++ BoaViagem/src/com/progdan/boaviagem/ViagemActivity.java
@@ -0,0 +1,12 @@
+package com.progdan.boaviagem;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ViagemActivity extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.viagem);
+	}
+}
